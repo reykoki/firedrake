ed4ceef86359f42d897c0248d2c01c9641968204
reykoki <reykokimonster@gmail.com>
2021-04-29 10:54:40 -0600

    perf

--- reytest.py
+++ reytest.py
@@ -1,6 +1,10 @@
 from firedrake import *
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(8, 8), dpi=80)
 n = 30
 mesh = UnitSquareMesh(n, n)
+fig.axes = plt.subplots()
+triplot(mesh, axes=axes)
 
-V = VectorFunctionSpace(mesh, "CG", 2)
-V_out = VectorFunctionSpace(mesh, "CG", 1)
+
